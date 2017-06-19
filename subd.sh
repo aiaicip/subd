9f885137e8c50e1b5cd346d992513bfe94ae75c4
aiaicip <noorlegiman1@gmail.com>
2017-06-19 15:00:56 +0800

    Create subd.sh

---
+++ subd.sh
@@ -0,0 +1,14 @@
+apt-get update
+apt-get install git -y
+apt-get install python-pip -y
+cd
+pip install requests
+cd
+git clone https://github.com/laramies/theHarvester.git
+ls
+cd theHarvester/
+ls
+chmod +x theHarvester.py
+ls
+./theHarvester.py
+rm subd.sh
