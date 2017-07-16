74a8f5ce6f75a1e66b47b0a0dbbfd6e8f9324e4f
aiaicip <noorlegiman1@gmail.com>
2017-07-16 23:43:01 +0800

    Update and rename subd.sh to thv.sh

--- thv.sh
+++ thv.sh
@@ -1,14 +1,24 @@
+#!/bin/bash
+
 apt-get update
+
 apt-get install git -y
+clear
+
 apt-get install python-pip -y
-cd
+
 pip install requests
-cd
+
 git clone https://github.com/laramies/theHarvester.git
+
 ls
+
 cd theHarvester/
+
 ls
+
 chmod +x theHarvester.py
+
 ls
+
 ./theHarvester.py
-rm subd.sh
