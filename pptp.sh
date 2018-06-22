f5462848f915ac05d8d797437e85015f079e06f5
aiaicip <noorlegiman1@gmail.com>
2018-06-22 23:15:35 +0800

    Update pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -1,25 +1,58 @@
-#! /bin/bash
-apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-apt-get upgrade -y
-apt-get install pptpd -y
-apt-get install curl -y
-apt-get update -y
-apt-get upgrade -y
-update-rc.d pptpd defaults
-echo "localip 172.20.1.1" >> /etc/pptpd.conf
-echo "remoteip 172.20.1.2-254" >> /etc/pptpd.conf
+#!/bin/bash
+
+echo "###################################"
+echo "This script will configure PPTP VPN"
+echo "###################################"
+
+# Install PPTP daemon service
+apt-get -y install pptpd
+
+# Set DNS
 echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
 echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
-echo "user  	*	   abc123	   *" >> /etc/ppp/chap-secrets 
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
-service pptpd restart
-apt-get install iptables-persistent -y
-iptables -I INPUT -p tcp --dport 1723 -m state --state NEW -j ACCEPT
-iptables -I INPUT -p gre -j ACCEPT
-iptables -t nat -I POSTROUTING -o eth1 -j MASQUERADE
-iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -s 172.20.1.0/24 -j TCPMSS  --clamp-mss-to-pmtu
-service iptables-persistent save
-iptables-save >> iptables
 
+# Get server IP
+ip=`ifconfig eth0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+
+# Set IP for server & user
+echo "localip $ip" >> /etc/pptpd.conf
+echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
+
+# Input username and password
+echo "###################################"
+echo "Please enter your new login details"
+echo "###################################"
+echo "Enter new username: "
+read username
+echo "Enter new password: "
+read password
+
+# Set user & password
+# Replace $username and $password
+echo "$username    pptpd   $password  *" >> /etc/ppp/chap-secrets
+
+# Restart PPTPD to reload all config
+service pptpd restart
+
+# Forward packet between localip and remoteip
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+
+# Restart sysctl to reload config
+sysctl -p
+
+# Allow PPTP traffic on iptables
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A INPUT -p 47 -j ACCEPT
+
+# Save iptable
+iptables-save
+
+echo "############################"
+echo "PPTP VPN Login Details"
+echo "############################"
+echo "Your IP is " $ip
+echo "Your username is " $username
+echo "Your password is " $password
+echo "############################"
+
+exit
