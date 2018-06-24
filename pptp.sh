a126e7b245309742429770fc580a951424dd4a33
aiaicip <noorlegiman1@gmail.com>
2018-06-24 15:05:59 +0800

    Update pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -5,34 +5,57 @@ echo "This script will configure PPTP VPN"
 echo "###################################"
 
 # Install PPTP daemon service
-apt-get -y install pptpd
+apt-get -y update;
+apt-get -y install pptpd;
 
 # Set DNS
 echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
 echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
 
 # Get server IP
-ip=`ifconfig eth0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+apt-get -y install wget || {
+  echo "Could not install wget, required to retrieve your IP address." 
+  exit 1
+}
+
+# Find out external ip 
+IP=wget -q -O - http://api.ipify.org
+
+if [ "x$IP" = "x" ]
+then
+  echo "============================================================"
+  echo "  !!!  COULD NOT DETECT SERVER EXTERNAL IP ADDRESS  !!!"
+else
+  echo "============================================================"
+  echo "Detected your server external ip address: $IP"
+fi
 
 # Set IP for server & user
-echo "localip $ip" >> /etc/pptpd.conf
-echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
+echo "localip $IP" >> /etc/pptpd.conf
+echo "remoteip 10.1.0.100-199" >> /etc/pptpd.conf
 
-# Input username and password
-echo "###################################"
-echo "Please enter your new login details"
-echo "###################################"
-echo "Enter new username: "
-read username
-echo "Enter new password: "
-read password
+# Create User and Random Password
+LEN=$(echo ${#PASS})
 
-# Set user & password
-# Replace $username and $password
-echo "$username    pptpd   $password  *" >> /etc/ppp/chap-secrets
+if [ -z "$PASS" ] || [ $LEN -lt 8 ] || [ -z "$NAME"]
+then
+   P1=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+   P2=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+   P3=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+   PASS="$P1-$P2-$P3"
+fi
 
-# Restart PPTPD to reload all config
-service pptpd restart
+if [ -z "$NAME" ]
+then
+   NAME="vpn"
+fi
+
+cat >/etc/ppp/chap-secrets <<END
+# Secrets for authentication using CHAP
+# client server secret IP addresses
+$NAME pptpd $PASS *
+END
+
 
 # Forward packet between localip and remoteip
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
@@ -41,18 +64,33 @@ echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 
 # Allow PPTP traffic on iptables
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A INPUT -p 47 -j ACCEPT
+iptables -P FORWARD ACCEPT
+iptables --table nat -A POSTROUTING -o venet0 -j MASQUERADE
 
 # Save iptable
 iptables-save
 
-echo "############################"
-echo "PPTP VPN Login Details"
-echo "############################"
-echo "Your IP is " $ip
-echo "Your username is " $username
-echo "Your password is " $password
-echo "############################"
+# Auto reload iptables
+cat >> /etc/network/if-pre-up.d/iptablesload << END
+#!/bin/sh
+iptables-restore < /etc/iptables.rules
+exit 0
 
-exit
+END
+sh /etc/network/if-pre-up.d/iptablesload
+
+#Execute iptables
+chmod +x /etc/network/if-pre-up.d/iptablesload
+
+
+# User details
+echo   "Server       =$IP"
+echo   "VPN username = $NAME"
+echo   "password     = $PASS"
+echo   "============================================================"
+sleep 2
+
+# Restart PPTPD to reload all config
+service pptpd restart
+
+exit 0
