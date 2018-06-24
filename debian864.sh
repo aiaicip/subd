9692f12e3a3bb776026d67554db35578eb855400
aiaicip <noorlegiman1@gmail.com>
2018-06-24 22:17:28 +0800

    Update debian864.sh

--- debian864.sh
+++ debian864.sh
@@ -93,6 +93,9 @@ echo "
 UPDATE AND UPGRADE PROCESS
 
 "
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+apt-get -y autoremove
 
 echo "
 INSTALL COMMANDS
@@ -150,9 +153,6 @@ cd ddos-deflate-master && ./install.sh
 service exim4 stop;sysv-rc-conf exim4 off 
 
 # install webmin
-sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
-wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
-apt-get -y autoremove
 apt-get -y install webmin
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
