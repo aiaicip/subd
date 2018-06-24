89a96b81a0b542953a640a53b707228adfc10918
aiaicip <noorlegiman1@gmail.com>
2018-06-24 22:14:47 +0800

    Update debian864.sh

--- debian864.sh
+++ debian864.sh
@@ -152,8 +152,8 @@ service exim4 stop;sysv-rc-conf exim4 off
 # install webmin
 sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
 wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
-apt-get -y autoremove;
-apt-get -y install webmin;
+apt-get -y autoremove
+apt-get -y install webmin
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
 #ssh banner
